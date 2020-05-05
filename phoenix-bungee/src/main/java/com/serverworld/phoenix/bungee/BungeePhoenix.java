44c03107662f13a0ae1da6d4ede676a94f24b9ec
paul <paul002706@gmail.com>
2020-05-05 15:54:52 +0800

    .

--- phoenix-bungee/src/main/java/com/serverworld/phoenix/bungee/BungeePhoenix.java
+++ phoenix-bungee/src/main/java/com/serverworld/phoenix/bungee/BungeePhoenix.java
@@ -1,5 +1,24 @@
+/*
+ * WorldMISF - cms of mc-serverworld
+ * Copyright (C) 2019-2020 mc-serverworld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.serverworld.phoenix.bungee;
 
+import com.serverworld.phoenix.bungee.commands.BungeePhoenixCommands;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
@@ -19,6 +38,16 @@ public class BungeePhoenix extends Plugin {
     public static Connection connection;
     @Override
     public void onEnable() {
+        setupconfig();
+
+
+        getLogger().info("Yay! It loads!");
+        getLogger().info("Helloworld");
+
+        getProxy().getPluginManager().registerCommand(this,new BungeePhoenixCommands(this));
+    }
+
+    public void setupconfig(){
         if (!getDataFolder().exists())
             getDataFolder().mkdir();
 
@@ -36,12 +65,6 @@ public class BungeePhoenix extends Plugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         config = new BungeePhoenixConfig(this);
-        getLogger().info("Yay! It loads!");
-        getLogger().info("Helloworld");
-
-        getProxy().getPluginManager().registerCommand(this,new banplayer(this));
-        getProxy().getPluginManager().registerCommand(this,new unbanplayer(this));
     }
 }
---
+++ phoenix-bungee/src/main/java/com/serverworld/phoenix/bungee/commands/BungeePhoenixCommands.java
@@ -0,0 +1,83 @@
+/*
+ * WorldMISF - cms of mc-serverworld
+ * Copyright (C) 2019-2020 mc-serverworld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.serverworld.phoenix.bungee.commands;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+import java.util.*;
+
+public class BungeePhoenixCommands extends Command implements TabExecutor{
+    public BungeePhoenixCommands(Plugin plugin){
+        super("misfbungeecord","misfbungeecord");
+    }
+
+    //dan playername endtime reason
+    public void execute(CommandSender commandSender, String[] strings) {
+
+    }
+
+    public static boolean startsWithIgnoreCase(String s, String start) {
+        return s.regionMatches(true, 0, start, 0, start.length());
+    }
+
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        List<String> completions = new ArrayList<>();
+        List<String> commands = new ArrayList<>();
+
+        if (args.length == 1) {
+            if (sender.hasPermission("misfbungeecord.command.help"))
+                commands.add("help");
+            if (sender.hasPermission("misfbungeecord.command.info"))
+                commands.add("info");
+            if (sender.hasPermission("misfbungeecord.command.sudo.setup"))
+                commands.add("setup");
+            if (sender.hasPermission("misfpaperspigot.command.login"))
+                commands.add("login");
+
+            for (String subCmd : commands)
+                if (startsWithIgnoreCase(subCmd, args[0])) completions.add(subCmd);
+        }/*else if (args.length == 2) {
+
+            if (args[0].equals("set")) {
+                if (sender.hasPermission("misfpaperspigot.command.set.time"))
+                    commands.add("time");
+            }
+
+            for (String subCmd : commands)
+                if (startsWithIgnoreCase(subCmd, args[1])) completions.add(subCmd);
+        } else if (args.length == 3) {
+
+            if (args[0].equals("set")&&args[1].equals("weather")) {
+                commands.add("clear");
+            } else if (args[0].equals("set")&&args[1].equals("time")){
+                commands.add("day");
+            }
+
+            for (String subCmd : commands)
+                if (startsWithIgnoreCase(subCmd, args[2])) completions.add(subCmd);
+        }*/
+        Collections.sort(completions);
+        return completions;
+    }
+}
