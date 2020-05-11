efd849e86022e0b3b1e2fdfc8070735b8c706eab
admin <paul002706@gmail.com>
2020-05-11 15:17:32 +0800

    .

--- worldUserProfile/src/main/java/com/serverworld/worldUserProfile/bungeecord/Listeners/PlayerLogin.java
+++ worldUserProfile/src/main/java/com/serverworld/worldUserProfile/bungeecord/Listeners/PlayerLogin.java
@@ -1,20 +1,10 @@
 package com.serverworld.worldUserProfile.bungeecord.Listeners;
 
-import com.serverworld.worldUserProfile.bungeecord.BungeeworldUserProfile;
-import com.serverworld.worldUserProfile.bungeecord.BungeeSqlDatabase;
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 public class PlayerLogin implements Listener {
@@ -25,15 +15,6 @@ public class PlayerLogin implements Listener {
 
     @EventHandler
     public void onPostLogin(PostLoginEvent event) {
-        try {
-            Statement statement = BungeeSqlDatabase.connection.createStatement();
-
-
-            ResultSet rs = statement.executeQuery("SELECT PlayerUUID from worldprofile_userlang WHERE PlayerUUID='"+event.getPlayer().getUniqueId()+"';");
-            Boolean playerexists =false;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
 }
