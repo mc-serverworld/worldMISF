43851f71564590eb0377bfa3e92432029a7183f5
admin <paul002706@gmail.com>
2020-06-11 16:05:44 +0800

    .

--- phoenix-bungee/src/main/java/com/serverworld/phoenix/bungee/Listeners/Messagecoming.java
+++ phoenix-bungee/src/main/java/com/serverworld/phoenix/bungee/Listeners/Messagecoming.java
@@ -20,6 +20,7 @@
 
 package com.serverworld.phoenix.bungee.Listeners;
 
+import com.google.gson.internal.$Gson$Preconditions;
 import com.serverworld.phoenix.bungee.BungeePhoenix;
 import com.serverworld.phoenix.bungee.util.DebugMessage;
 import com.serverworld.worldSocket.bungeecord.events.MessagecomingEvent;
@@ -28,7 +29,7 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class Messagecoming implements Listener {
+public class Messagecoming extends MessagecomingEvent implements Listener {
 
     public Messagecoming(){
         DebugMessage.sendInfo("hello");
